d39d729b49bc9fea73602c27add80c0f645cb108
Otavio Leite <otaviofl@gmail.com>
2018-12-05 14:14:32 -0200

    assigned the relationship between models

--- app/models/picture_experience.rb
+++ app/models/picture_experience.rb
@@ -1,3 +1,5 @@
 class PictureExperience < ApplicationRecord
   belongs_to :experience
+  mount_uploader :picture, PhotoUploader
+
 end
--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,4 +1,5 @@
 class Supplier < ApplicationRecord
+  has_many :experiences
   mount_uploader :picture_avatar, PhotoUploader
   mount_uploader :picture_banner, PhotoUploader
 end
