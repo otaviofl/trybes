bc9b411a2f8183fcd5731e543a74dbbe9efe99b6
Otavio Leite <otaviofl@gmail.com>
2018-12-06 12:13:54 -0200

    created routes to index and show and controller for experiences

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  resources :experiences
   devise_for :users
   root to: 'pages#home'
+
+  resources :experiences, only: [:index, :show, :new, :create] do
+    resources :ratings, only: [:create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
