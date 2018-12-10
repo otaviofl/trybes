b55f2ff82ca59e8e97c7371a060d2f645f0fe31b
otavioleite <otaviofl@gmail.com>
2018-12-10 17:11:25 -0200

    new seed file architecture - ready for populating with data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_05_162342) do
+ActiveRecord::Schema.define(version: 2018_12_10_163657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,10 @@ ActiveRecord::Schema.define(version: 2018_12_05_162342) do
     t.float "latitude"
     t.float "longitude"
     t.string "address"
+    t.string "title"
+    t.string "short_title"
+    t.integer "vacancy"
+    t.string "short_description"
     t.index ["category_id"], name: "index_experiences_on_category_id"
     t.index ["supplier_id"], name: "index_experiences_on_supplier_id"
   end
@@ -80,6 +84,9 @@ ActiveRecord::Schema.define(version: 2018_12_05_162342) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "location"
+    t.string "phone"
+    t.string "facebook"
+    t.string "web_address"
   end
 
   create_table "users", force: :cascade do |t|
