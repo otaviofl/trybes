a7cdf3ef880602b41c4dffee3f7e98a0642dcb1c
claudio victer <cvicter@outlook.com>
2018-12-12 18:28:08 -0200

    bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,36 @@
 class BookingsController < ApplicationController
+
+  def index
+    @bookings = Booking.all
+  end
+
+  def new
+    @booking = Booking.new
+    @experience = Experience.find(params[:experience_id])
+    @user = current_user
+  end
+
+  def create
+    @experience = Experience.find(params[:experience])
+    @user = User.find(params[:user])
+    @booking = Booking.new(user: @user,
+                           experience: @experience
+                           )
+    if @booking.save!
+      flash[:notice] = "Booking successfully created"
+      redirect_to root_path
+    else
+      flash[:error] = @booking.errors.full_messages.first
+      redirect_to @booking
+
+    end
+
+  end
+
+
+  private
+
+  def booking_params
+      params.require(:booking).permit(:experience_id, :user_id)
+  end
 end
