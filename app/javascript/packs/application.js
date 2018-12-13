8242efb53143a330c58f4013aadb70db0a356e90
renato pinto <renatopeixoto.rj@gmail.com>
2018-12-13 18:22:32 -0200

    Booking confirmation modal page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,3 +5,20 @@ initUpdateNavbarOnScroll();
 
 import { initUpdateBookbarOnScroll } from '../components/book-bar-match';
 initUpdateBookbarOnScroll();
+
+
+$(document).ready(function() {
+  var $amount = $("#amount").html();
+  $("button").click(function() {
+    var id = this.id
+    if (id === "minus" && $amount > 0) {
+      $amount--;
+      $("#amount").html($amount);
+    }
+    else if (id === "plus") {
+      $amount++;
+      $("#amount").html($amount);
+    }
+
+  });
+});
