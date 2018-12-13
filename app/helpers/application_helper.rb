5018f0c09c480d144379ef0a3524e9bd14d0d022
renato pinto <renatopeixoto.rj@gmail.com>
2018-12-13 10:51:30 -0200

    Login_SignUp Layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def experiences_show?
     action_name == 'show' && controller_name == 'experiences'
   end
+
+  def sessions_new?
+    action_name == 'new' && controller_name == 'sessions'
+  end
+
+  def registrations_new?
+    action_name == 'new' && controller_name == 'registrations'
+  end
 end
